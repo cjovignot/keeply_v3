7854ddcea5a59087bc55226b351cd09562195590
cjovignot <cosme.jovignot@epitech.eu>
2025-12-06 21:32:02 +0100

    [VERCEL] Config

--- frontend/api/hello.ts
+++ frontend/api/hello.ts
@@ -1,5 +1,5 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
+// api/hello.ts
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ message: "Hello from API!" });
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from Vercel API!" });
 }
