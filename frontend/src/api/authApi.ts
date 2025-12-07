6452fb1e7e7a74b06e2d7e7f023b54f3138f6c46
cjovignot <cosme.jovignot@epitech.eu>
2025-12-07 12:45:03 +0100

    [FIX] connectDB google-login

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -3,19 +3,19 @@ import axiosClient from "./axiosClient";
 export const authApi = {
   // 🔹 Login classique
   login: (data: { email: string; password: string }) =>
-    axiosClient.post("/auth/login", data), // ✅ slash devant
+    axiosClient.post("api/auth/login", data), // ✅ slash devant
 
   // 🔹 Signup public
   signup: (data: { name: string; email: string; password: string }) =>
-    axiosClient.post("/auth/signup", data),
+    axiosClient.post("api/auth/signup", data),
 
   // 🔹 Login Google (token envoyé par Google)
   googleLogin: (credential: string) =>
-    axiosClient.post("/auth/google-login", { token: credential }),
+    axiosClient.post("api/auth/google-login", { token: credential }),
 
   // 🔹 Récupération du user connecté
-  getMe: () => axiosClient.get("/auth/me"),
+  getMe: () => axiosClient.get("api/auth/me"),
 
   // 🔹 Logout
-  logout: () => axiosClient.post("/auth/logout"),
+  logout: () => axiosClient.post("api/auth/logout"),
 };
