02502ba257d7a17f7ca6028713adad0ae7ed7d5e
cjovignot <cosme.jovignot@epitech.eu>
2025-12-09 08:37:21 +0100

    [UI] scroll on top on mount

--- frontend/src/components/PageWrapper.tsx
+++ frontend/src/components/PageWrapper.tsx
@@ -1,11 +1,20 @@
 import { motion } from "framer-motion";
 import type { ReactNode } from "react";
+import { useEffect } from "react";
 
 type Props = {
   children: ReactNode;
 };
 
 const PageWrapper = ({ children }: Props) => {
+  // Scroll tout en haut au chargement de la page
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant", // ou "smooth" selon tes préférences
+    });
+  }, []);
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 20 }}
