7eda5f27722e03f770b50bf7721495e136ec7f0f
cjovignot <cosme.jovignot@epitech.eu>
2025-12-09 08:17:28 +0100

    [ADD] Home footer links

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -149,7 +149,7 @@ export default function Home() {
 <footer className="w-full mt-20 px-6">
   <div className="max-w-4xl mx-auto py-10">
 
-    <div className="grid grid-cols-3 justify-center gap-4 text-gray-400 text-sm mb-6">
+    <div className="flex justify-center gap-4 text-gray-400 text-sm mb-6">
       <Link to="/regles_de_confidentialite" className="hover:text-yellow-400 transition-colors">
         Confidentialité
       </Link>
