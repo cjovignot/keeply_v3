77567e19417a5987d5696e75c93bb5a981d0b639
cjovignot <cosme.jovignot@epitech.eu>
2025-12-14 10:38:22 +0100

    [REFACTOR] Test preSplash

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,9 @@ import App from "./App";
 import { AuthProvider } from "./contexts/AuthProvider";
 import { PrintProvider } from "./contexts/PrintProvider.tsx";
 
+const el = document.getElementById("pre-splash");
+if (el) el.remove();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // Tu peux laisser React.StrictMode en DEV, ça double les effets intentionnellement
   <React.StrictMode>
